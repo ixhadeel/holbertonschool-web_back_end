186b0a1a5d570787dbfb04cff176f90caac281c5
Hadeel <hadeel.alqhtani206@gmail.com>
2026-09-12 21:08:04 +0300

    Fix hypermedia deletion pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination."""
+"""
+Deletion-resilient hypermedia pagination
+"""
 
 import csv
+import math
 from typing import List, Dict
 
 
@@ -11,12 +14,12 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initialize datasets."""
+        """Initialize dataset variables."""
         self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Return cached dataset."""
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -26,9 +29,10 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Return dataset indexed by position."""
+        """Dataset indexed by sorting position, starting at 0."""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
@@ -38,22 +42,19 @@ class Server:
     def get_hyper_index(self, index: int = None,
                         page_size: int = 10) -> Dict:
         """Return a deletion-resilient page."""
-        assert index is None or (
-            isinstance(index, int)
-            and 0 <= index < len(self.dataset())
-        )
-
         if index is None:
             index = 0
 
+        assert isinstance(index, int)
+        assert 0 <= index < len(self.dataset())
+
         data = []
-        indexed_data = self.indexed_dataset()
+        indexed_dataset = self.indexed_dataset()
         next_index = index
 
         while len(data) < page_size:
-            if next_index in indexed_data:
-                data.append(indexed_data[next_index])
-
+            if next_index in indexed_dataset:
+                data.append(indexed_dataset[next_index])
             next_index += 1
 
         return {
